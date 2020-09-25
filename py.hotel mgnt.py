beed692c13448eedc0242acf9dfa7e151657dfb9
mustaphamichaelaziz <71891295+mustaphamichaelaziz@users.noreply.github.com>
2020-09-25 16:04:30 +0000

    added new files

---
+++ py.hotel mgnt.py
@@ -0,0 +1,152 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:57:54) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> 
+======== RESTART: C:/Users/Mustapha/Desktop/SOURCE CODE/py.hotel mgnt.py =======
+
+
+*****WELCOME TO HOTEl DE SUAREZ*****
+
+1.Enter Customer Data
+2.Calculate Room Rent
+3.Calculate Food Purchased
+4.Show total cost
+5.EXIT
+
+Enter the number of your choice:1
+
+Enter your Fullname:Sahid Dauda Sankoh
+
+Enter your address:Main Road Congo Cross
+
+Enter your check in date:12/08/2020
+
+Enter your checkout date:29/08/2020
+Your room no.: 1 
+
+1.Enter Customer Data
+2.Calculate Room Rent
+3.Calculate Food Purchased
+4.Show total cost
+5.EXIT
+
+Enter the number of your choice:2
+We have the following rooms for you:-
+1.  Class A---->4000
+2.  Class B---->3000
+3.  Class C---->2000
+4.  Class D---->1000
+Enter the number of your choice Please->3
+For How Many Nights Did You Stay:10
+you have choose room Class C
+your choosen room rent is = 20000 
+
+1.Enter Customer Data
+2.Calculate Room Rent
+3.Calculate Food Purchased
+4.Show total cost
+5.EXIT
+
+Enter the number of your choice:4
+******HOTEL BILL******
+Customer details:
+Customer name: Sahid Dauda Sankoh
+Customer address: Main Road Congo Cross
+Check in date: 12/08/2020
+Check out date 29/08/2020
+Room no. 1
+Your Room rent is: 20000
+Your Food bill is: 0
+Your sub total Purchased is: 20000
+Additional Service Charges is 1000
+Your grandtotal Purchased is: 21000 
+
+1.Enter Customer Data
+2.Calculate Room Rent
+3.Calculate Food Purchased
+4.Show total cost
+5.EXIT
+
+Enter the number of your choice:7
+1.Enter Customer Data
+2.Calculate Room Rent
+3.Calculate Food Purchased
+4.Show total cost
+5.EXIT
+
+Enter the number of your choice:8
+1.Enter Customer Data
+2.Calculate Room Rent
+3.Calculate Food Purchased
+4.Show total cost
+5.EXIT
+
+Enter the number of your choice:9
+1.Enter Customer Data
+2.Calculate Room Rent
+3.Calculate Food Purchased
+4.Show total cost
+5.EXIT
+
+Enter the number of your choice:10
+1.Enter Customer Data
+2.Calculate Room Rent
+3.Calculate Food Purchased
+4.Show total cost
+5.EXIT
+
+Enter the number of your choice:1
+
+Enter your Fullname:Mustapha B. Sankoh
+
+Enter your address:89 main road congo cross, Freetown
+
+Enter your check in date:10/08/2020
+
+Enter your checkout date:30/08/2020
+Your room no.: 2 
+
+1.Enter Customer Data
+2.Calculate Room Rent
+3.Calculate Food Purchased
+4.Show total cost
+5.EXIT
+
+Enter the number of your choice:2
+We have the following rooms for you:-
+1.  Class A---->4000
+2.  Class B---->3000
+3.  Class C---->2000
+4.  Class D---->1000
+Enter the number of your choice Please->3
+For How Many Nights Did You Stay:20
+you have choose room Class C
+your choosen room rent is = 40000 
+
+1.Enter Customer Data
+2.Calculate Room Rent
+3.Calculate Food Purchased
+4.Show total cost
+5.EXIT
+
+Enter the number of your choice:4
+******HOTEL BILL******
+Customer details:
+Customer name: Mustapha B. Sankoh
+Customer address: 89 main road congo cross, Freetown
+Check in date: 10/08/2020
+Check out date 30/08/2020
+Room no. 2
+Your Room rent is: 40000
+Your Food bill is: 0
+Your sub total Purchased is: 40000
+Additional Service Charges is 1000
+Your grandtotal Purchased is: 41000 
+
+1.Enter Customer Data
+2.Calculate Room Rent
+3.Calculate Food Purchased
+4.Show total cost
+5.EXIT
+
+Enter the number of your choice:
